6d2ccd98efbe4ad457bada2176f198f19edc712f
Suchita Manohare <100685855+Dinesh-Manohare@users.noreply.github.com>
2022-08-20 23:12:01 +0530

    Js added

--- script.js
+++ script.js
@@ -23,9 +23,22 @@ window.addEventListener("DOMContentLoaded", (event) => {
     }
   };
   document.addEventListener("scroll", navbarShrink);
-  
 
-  
+  // const mainNav = document.body.querySelector('#stickynav');
+  //   if (mainNav) {
+  //       new bootstrap.ScrollSpy(document.body, {
+  //           target: '#stickynav ul li a',
+  //           offset: 74,
+  //       });
+  //   };
+
+
 });
+// document.getElementById("li-about").scrollIntoView();
 
-
+// const about = document.body.querySelector("#li-about");
+// function handleClickAbout() {
+//   console.log("about click");
+//   document.getElementById("li-about").scrollIntoView();
+// }
+// about.addEventListener("click", handleClickAbout);
